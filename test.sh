85374742ba0a3b4e906ec2e8af471a434892a8fe
Nathan Rajlich <nathan@tootallnate.net>
2018-11-29 01:54:31 -0800

    Optimize `tree` for darwin build and dockerfile

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env import
 set -euo pipefail
 import "assert@2.1.3"
-import "tootallnate/static-binaries@0.0.5"
+import "tootallnate/static-binaries@0.0.6"
 
 static_binaries jq
 assert_equal "$IMPORT_CACHE/bin/jq" "$(which jq)"
 
 static_binaries socat
 assert_equal "$IMPORT_CACHE/bin/socat" "$(which socat)"
+
+static_binaries tree
+assert_equal "$IMPORT_CACHE/bin/tree" "$(which tree)"
