83a4978c7bc704d9b26e9cc96eeb25fc8731e10c
Nathan Rajlich <nathan@tootallnate.net>
2019-01-02 20:30:10 -0800

    Refactor to use `os_path_unshift` at import time
    This way there are no side-effects from `import static-binaries`.

--- static-binaries.sh
+++ static-binaries.sh
@@ -1,23 +1,20 @@
-import "os@0.0.3"
-
-if [ -z "${IMPORT_CACHE-}" ]; then
-  IMPORT_CACHE="$HOME/.import-cache"
-fi
-
-mkdir -p "$IMPORT_CACHE/bin"
-export PATH="$IMPORT_CACHE/bin:$PATH"
+import "os@0.1.0"
 
 static_binaries() {
   local path
+  local cache="${IMPORT_CACHE-${HOME}/.import-cache}"
+  mkdir -p "$cache/bin"
+  os_path_unshift "$cache/bin"
+
   path="$(print=1 import "./binaries/$(os_platform)/$(os_arch)/$1")"
   if [ ! -x "$path" ]; then
     [ -n "${IMPORT_DEBUG-}" ] && printf "static-binaries: " >&2
     chmod ${IMPORT_DEBUG:+-v} +x "$path" >&2
   fi
 
-  local bin="$IMPORT_CACHE/bin/$1"
+  local bin="$cache/bin/$1"
   if [ ! -e "$bin" ]; then
     [ -n "${IMPORT_DEBUG-}" ] && printf "static-binaries: creating symlink " >&2
-    ln -fs${IMPORT_DEBUG:+v} "..${path:${#IMPORT_CACHE}}" "$bin" >&2
+    ln -fs${IMPORT_DEBUG:+v} "..${path:${#cache}}" "$bin" >&2
   fi
 }
