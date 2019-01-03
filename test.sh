e7383b85de888a57b10e17b3e11579808013d03b
Nathan Rajlich <nathan@tootallnate.net>
2019-01-03 13:20:07 -0800

    1.0.0

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env import
 set -euo pipefail
 import "assert@2.1.3"
-import "static-binaries@0.0.6"
+import "static-binaries@1.0.0"
 
 static_binaries jq
 assert_equal "$IMPORT_CACHE/bin/jq" "$(which jq)"
